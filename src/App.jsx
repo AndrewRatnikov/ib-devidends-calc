d7317b1055ca488d23ac9f7728f22e755d6696dd
Andrew <ratnikov.am@gmail.com>
2025-05-25 21:44:07 +0300

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { Input } from '@/src/components/ui/input';
+
 function App() {
   return (
-    <>
-      <h1>Devidend calculator</h1>
-    </>
+    <main>
+      <div>
+        <h1 className="text-5xl text-center">Tax Dividend calculator</h1>
+
+        <p className="text-center">Description</p>
+      </div>
+
+      <div>
+        <form>
+          <Input type="file" />
+        </form>
+      </div>
+    </main>
   );
 }
 
